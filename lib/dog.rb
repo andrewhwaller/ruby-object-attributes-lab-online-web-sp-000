89a1b4b311f6aaabdec5945b57c528195ad94c53
Andrew Waller <andrewhwaller@gmail.com>
2019-03-16 16:05:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,19 @@
 class Dog
 
   def name
-    this_dogs_name
+    @this_dogs_name
   end
 
   def name=(dog_name)
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
 
   def breed
-    this_dogs_breed
+    @this_dogs_breed
   end
 
   def breed=(dog_breed)
-    this_dogs_breed = dog_breed
+    @this_dogs_breed = dog_breed
   end
 
 end
